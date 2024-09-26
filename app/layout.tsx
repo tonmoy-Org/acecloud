9059cac4965ba26a3f04ec5ad68bacf7eca317ad
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2024-09-26 22:40:02 +0600

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        {children}
+        <div className="min-h-screen">
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
