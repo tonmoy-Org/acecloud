69f993c62ee308107f554c8090924c91cc7c471f
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2024-10-02 00:17:06 +0600

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      deviceSizes: [640, 768, 1024, 1280, 1600],
+    },
+    compress: true,
+    staticPageGenerationTimeout: 60,
+    swcMinify: true,
+    experimental: {
+      scrollRestoration: true,
+    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: '/(.*)',
+    //       headers: [
+    //         {
+    //           key: 'Cache-Control',
+    //           value: 'public, max-age=31536000, immutable',
+    //         },
+    //       ],
+    //     },
+    //   ];
+    // },
+  };
+  
+  export default nextConfig;
+  
