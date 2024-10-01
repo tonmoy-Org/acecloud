b71c1871017d04779bfc519ddba1eef616bd67f1
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2024-10-02 00:24:08 +0600

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-      deviceSizes: [640, 768, 1024, 1280, 1600],
-    },
-    compress: true,
-    staticPageGenerationTimeout: 60,
-    swcMinify: true,
-    experimental: {
-      scrollRestoration: true,
-    },
-    // async headers() {
-    //   return [
-    //     {
-    //       source: '/(.*)',
-    //       headers: [
-    //         {
-    //           key: 'Cache-Control',
-    //           value: 'public, max-age=31536000, immutable',
-    //         },
-    //       ],
-    //     },
-    //   ];
-    // },
+    swcMinify: true, // SWC minification for faster builds
   };
   
   export default nextConfig;
